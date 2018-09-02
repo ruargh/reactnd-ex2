7b79cc7183f52b6f7910c4c376c44f4f07020d22
Ruargh <ruargh@gmail.com>
2018-09-02 09:03:45 +0200

    task: Add component - Movie

---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Movie extends Component {
+  render() {
+    return(
+      <div>
+        <h2>Forrest Gump</h2>
+        <p>Liked By:</p>
+        <ul>
+          <li>Nicholas Lain</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Movie
