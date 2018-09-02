3f38b49249638fc4a4516b787c2fb47dfa2ea547
root <ruargh@gmail.com>
2018-09-02 06:49:50 +0000

    add component

---
+++ src/PopularMovies.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class PopularMovies extends Component {
+  render() {
+    return (
+      <ul>
+        <h2>Forrest Gump</h2>
+        <p>Liked By:</p>
+        <ul>
+          <li>Nicholas Lain</li>
+        </ul>
+      </ul>   
+      )
+  }
+}
+
+export default PopularMovies
